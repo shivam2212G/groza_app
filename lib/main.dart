526857616ec193d7dc0621593024ae699e3e63c9
Shivam <bulletspeed2005@gmail.com>
2025-06-21 10:49:12 +0530

    All Code Worked

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home:HomeScreen(),
-        debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: true,
       ),
     );
   }
